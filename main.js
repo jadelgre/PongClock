729f14a791231d1190dc774efbdfcf2db2d96e7c
Jacob Adelgren <jadelgre@mymail.mines.edu>
2015-07-31 22:22:00 -0500

    added ball object

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var leftPaddle;
 var rightPaddle;
 var ball;
 
-var paddle = function(width, height, xpos) {
+var pong_paddle = function(width, height, xpos) {
 	this.width = width / 30;
 	this.height = height / 5;
 	this.x = xpos;
@@ -22,13 +22,11 @@ var paddle = function(width, height, xpos) {
 	};
 	
 	this.clear = function() {
-		//context.fillStyle = background; // set the fill style to the current background color
 		context.clearRect(this.x, this.y,this.width,this.height); // draw a rectangle over the current one to clear it
-		//context.fill();
 	};
 	
 	this.draw = function() {
-		context.fillStyle = "yellow";
+		context.fillStyle = "white";
 		context.beginPath();
 		context.rect(this.x,this.y,this.width,this.height);
 		context.closePath();
@@ -36,17 +34,49 @@ var paddle = function(width, height, xpos) {
 	}
 }
 
+var pong_ball = function(width, height) {
+	this.x = width / 2;
+	this.y = width / 2;
+	this.dx = width / 200;
+	this.dy = width / 200;
+	this.xSize = 50; // width / 200;
+	this.ySize = 50; // width / 200;
+	this.update = function() {
+		this.clear();
+		this.x += this.dx;
+		//this.y += this.dy;
+		this.draw();
+	};
+	this.clear = function() { 
+		context.clearRect(this.x, this.y,this.xSize,this.ySize); // draw a rectangle over the current one to clear it
+	};
+
+	this.draw = function() {
+		context.fillStyle = "white";
+		context.beginPath();
+		context.rect(this.x,this.y,this.xSize,this.ySize);
+		context.closePath();
+		context.fill();
+	}
+
+}
+
 var update = function() {
 	leftPaddle.update();
+	rightPaddle.update();
+	ball.update();
 }
 
 window.onload = function() {
+	// get the canvas and context for drawing graphics
 	canvas = document.getElementById("game");
 	canvas.style.backgroundColor = background;
 	context = canvas.getContext( '2d' ); 
 	width = canvas.width;
 	height = canvas.height;
-	leftPaddle = new paddle(width, height, width / 20);
+	// instantiate the game components
+	leftPaddle = new pong_paddle(width, height, width / 20);
+	rightPaddle = new pong_paddle(width, height, width - (width / 20));
+	ball = new pong_ball(width, height);
 	setInterval( update, 1000 / fps );
-	console.log(test)
 }
