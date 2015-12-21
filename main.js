02db5fed238893c2b100388a5bc7417cb9ea0a5f
Jacob Adelgren <admin@admins-MacBook-Pro.local>
2015-12-21 11:53:24 -0700

    added new logic for incrementing score based upon clock. issues with right paddle not moving randomly

--- main.js
+++ main.js
@@ -11,13 +11,17 @@ var canvasHeight = 500;
 var canvasWidth = 500;
 var gameBackgroundColor = "black";
 
+// variables that indicate whether a paddle needs to miss
+var incrementHour = false;
+var incrementMinute = false;
+
 var initialize_canvas = function() {
 	canvas = document.getElementById("game");
 	canvas.style.backgroundColor = gameBackgroundColor;
 	canvas.width = canvasWidth;
 	canvas.height = canvasWidth;
 	context = canvas.getContext('2d');
-} 
+};
 	
 var pongBall = {
 	dx: -10,
@@ -25,21 +29,21 @@ var pongBall = {
 	rad: 10,
 	x: canvasWidth / 2,
 	y: canvasHeight / 2
-}
+};
 
 var leftPaddle = { 
 	height: 100,
 	width: 10,
 	x: 20,
 	y: canvasHeight / 2
-}
+};
 
 var rightPaddle = {
 	x: canvasWidth - 20,
 	height: 100,
 	width: 10,
 	y: canvasHeight / 2
-}
+};
 
 var draw = function () {
 	// clear the board
@@ -84,32 +88,94 @@ var update = function () {
 	// update left paddle
 
 	// update right paddle
-	if( rightPaddle.y + rightPaddle.height / 2 + pongBall.dy < canvasHeight && rightPaddle.y - rightPaddle.height / 2 + pongBall.dy > 0 ) {
-		rightPaddle.y += pongBall.dy; 
-	}
+	// if( rightPaddle.y + rightPaddle.height / 2 + pongBall.dy < canvasHeight && rightPaddle.y - rightPaddle.height / 2 + pongBall.dy > 0 ) {
+	// 	rightPaddle.y += pongBall.dy; 
+	// }
 	
-	if( leftPaddle.y + leftPaddle.height / 2 + pongBall.dy < canvasHeight && leftPaddle.y - leftPaddle.height / 2 + pongBall.dy > 0 ) {
-		leftPaddle.y += pongBall.dy;
+	// if( leftPaddle.y + leftPaddle.height / 2 + pongBall.dy < canvasHeight && leftPaddle.y - leftPaddle.height / 2 + pongBall.dy > 0 ) {
+	// 	leftPaddle.y += pongBall.dy;
+	// }
+
+
+	if(pongBall.x < (canvasWidth / 2) && pongBall.dx < 0) { // if the ball is moving left
+		variance = pongBall.rad;
+		if(incrementHour) {
+			variance *= 4;
+		}
+		
+		if(pongBall.y <= leftPaddle.y) {
+			leftPaddle.y -= (leftPaddle.y - pongBall.y) / variance;
+		} else {
+			leftPaddle.y += (pongBall.y - leftPaddle.y) / variance;
+		}
+	} else if (pongBall.x > (canvasWidth / 2) && pongBall.dy > 0) {
+		if(incrementMinute) {
+			variance += 4;
+		}
+
+		if(pongBall.y <= rightPaddle.y) {
+			rightPaddle.y -= (rightPaddle.y - pongBall.y) / variance;
+		} else {
+			rightPaddle.y += (pongBall.y - rightPaddle.y) / variance;
+		}
 	}
+
+	// make sure the left paddle does not go off screen
+	if(leftPaddle.y - (leftPaddle.height / 2) <= 0) leftPaddle.y = leftPaddle.height / 2;
+	if(leftPaddle.y + (leftPaddle.height / 2) >= canvasHeight) leftPaddle.y = canvasHeight - (leftPaddle.height / 2);
+	// make sure the right paddle does not go off screen 
+	if(rightPaddle.y - (rightPaddle.height / 2) <= 0) rightPaddle.y = rightPaddle.height / 2;
+	if(rightPaddle.y + (rightPaddle.height / 2) >= canvasHeight) rightPaddle.y = canvasHeight - (rightPaddle.height / 2);
 
 	// move ball
 	pongBall.x += pongBall.dx;
 	pongBall.y += pongBall.dy;
 	// check for collision
-	if( pongBall.x + pongBall.rad >= canvasWidth // collision with right wall
-	|| pongBall.x - pongBall.rad <= 0 // collision with left wall
-	|| pongBall.x + pongBall.rad >= rightPaddle.x 
-	|| (pongBall.x - pongBall.rad <= leftPaddle.x+leftPaddle.width 
-		&& pongBall.y < leftPaddle.y + leftPaddle.height/2
-		&& pongBall.y > leftPaddle.y - leftPaddle.height/2)) pongBall.dx = -pongBall.dx;
+	if( pongBall.x + pongBall.rad >= canvasWidth ) { // collision with right wall
+		if(incrementMinute) {
+			incrementMinute = false;
+		} else {
+			console.log('ball hit right wall when it should not have');
+		}
+		pongBall.dx = -pongBall.dx;
+	} else if ( pongBall.x - pongBall.rad <= 0 ) {// collision with left wall
+		if(incrementHour) {
+			incrementHour = false;
+		} else {
+			console.log('ball hit left wall when it should not have');
+		}
+		pongBall.dx = -pongBall.dx;
+	} else if ( pongBall.x + pongBall.rad >= rightPaddle.x 
+				|| (pongBall.x - pongBall.rad <= leftPaddle.x+leftPaddle.width 
+				&& pongBall.y < leftPaddle.y + leftPaddle.height/2
+				&& pongBall.y > leftPaddle.y - leftPaddle.height/2)) {
+		pongBall.dx = -pongBall.dx;
+	}
 	// check for ball collision with the top and bottom
 	if( pongBall.y + pongBall.rad >= canvasHeight || pongBall.y - pongBall.rad <= 0 ) pongBall.dy = -pongBall.dy;
 	// redraw all elements
 	draw();
 }
 
+var clockCycle = function() {
+	var time = new Date();
+	var minutes = time.getMinutes();
+	var seconds = time.getSeconds()
+
+	if(seconds == 0) {
+		if(minutes == 0) {
+			// if seconds == 0 and minutes == 0 then it's the top of the hour
+			incrementHour = true;
+		} else {
+			// else it's just the start of a new minute
+			incrementMinute = true;
+		}
+	}
+}
+
 window.onload = function() {
 	initialize_canvas();
 	setInterval(update, 1000 / fps );
+	setInterval(clockCycle, 1000); // run every second
 }
 
